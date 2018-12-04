e3ae8bbd825d2ad33703997ef8471081e2be6947
Daniel\Home <danielschumi@bluewin.ch>
2018-12-04 16:13:37 +0100

    added shoot mechanism to Player

---
+++ Assets/AwesomeJumpNRun/Scripts/Shoot/MoveShoot.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveShoot : MonoBehaviour {
+    [SerializeField] private int speed;
+    [SerializeField] private int damage;
+
+    public int Damage
+    {
+        get
+        {
+            return damage;
+        }
+        private set
+        {
+            damage = value;
+        }
+    }
+
+    private void Start () {
+        Rigidbody rigBody = GetComponent<Rigidbody>();
+        rigBody.AddForce(transform.forward * speed);
+	}
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        Destroy(gameObject);
+    }
+}
