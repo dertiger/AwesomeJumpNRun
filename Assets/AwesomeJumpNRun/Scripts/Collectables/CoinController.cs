731d428ca07e784b0c62dc3e376e812889c7ded3
Cramery <rami-602@hotmail.com>
2018-12-04 10:31:26 +0100

    UpdateCoinController

--- Assets/AwesomeJumpNRun/Scripts/Collectables/CoinController.cs
+++ Assets/AwesomeJumpNRun/Scripts/Collectables/CoinController.cs
@@ -10,9 +10,9 @@ public class CoinController : MonoBehaviour {
 
     private void OnTriggerEnter()
     {
-        Debug.Log(coinData.Value); //To Get the Value of the Coin
+        //Debug.Log(coinData.Value); //To Get the Value of the Coin
         transform.Rotate(new Vector3(0, 30, 0) * Time.deltaTime); //This doesnt work idk why, trieed for 1h to fix, it just doesnt want to rotate, but I give up. If it doesnt want to move it shouldnt
-        Destroy(this.gameObject); //maybe Inactive instead of destroy?
+        //Destroy(this.gameObject); //Deactivated for test Rotation 
     }
 
 }
