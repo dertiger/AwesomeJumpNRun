0bf54820d5836d32199b3f6cd5f2c95c23e5ed11
Daniel\Home <danielschumi@bluewin.ch>
2018-12-04 19:31:06 +0100

    Grunts can now Shoot shooter is smaller

---
+++ Assets/AwesomeJumpNRun/Scripts/Shoot/AutoShoot.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AutoShoot : Shoot {
+	// Update is called once per frame
+	void Update () {
+        OnShoot();
+	}
+}
