5a7917b27527f2bb721d2d8a127d4965d284d3d0
Cramery <rami-602@hotmail.com>
2018-12-06 17:46:11 +0100

    cave in whole cave

--- Assets/AwesomeJumpNRun/Scripts/Manager/LightSwitchManager.cs
+++ Assets/AwesomeJumpNRun/Scripts/Manager/LightSwitchManager.cs
@@ -6,25 +6,22 @@ public class LightSwitchManager : MonoBehaviour {
 
     [SerializeField]
     public Light mylight;
-    public GameObject mydirectionLight; 
-    
+  
 
     private void OnTriggerEnter(Collider other)
     {
-        mydirectionLight = GameObject.Find("Directional Light");
+
         if (other.gameObject.CompareTag("Player"))
         {
-            if ((other.gameObject.transform.position.z) < (this.gameObject.transform.position.z))
-            {
-                mylight.GetComponent<Light>().intensity = 0.0f;
-                mydirectionLight.SetActive(false);
-                RenderSettings.ambientLight = Color.black;
-            }
-            else
-            {
-                mylight.GetComponent<Light>().intensity = 1.0f;
-                mydirectionLight.SetActive(true);
-            }
+            mylight.GetComponent<Light>().intensity = 0.0f;   
+        }
+    }
+
+    private void OnTriggerExit(Collider other) {
+
+        if (other.gameObject.CompareTag("Player"))
+        {
+            mylight.GetComponent<Light>().intensity = 1.0f;
         }
     }
 }
