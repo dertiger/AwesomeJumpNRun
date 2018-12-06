866b4f11b33e30fdfc0da8e9dacecc2c07ff3993
Cramery <rami-602@hotmail.com>
2018-12-06 14:16:04 +0100

    Add Light switch

---
+++ Assets/AwesomeJumpNRun/Scripts/Manager/LightSwitchManager.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LightSwitchManager : MonoBehaviour {
+
+    private void OnTriggerEnter(Collider other)
+    {
+        //If other is Player
+        //If player heads right
+        //darken
+        //if player heads left
+        //light up again
+    }
+}
