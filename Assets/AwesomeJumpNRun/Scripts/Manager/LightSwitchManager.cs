9da8f8a6e06328a95d7ac57cdd14095a33c676ad
Cramery <rami-602@hotmail.com>
2018-12-07 14:34:00 +0100

    Make it dark

--- Assets/AwesomeJumpNRun/Scripts/Manager/LightSwitchManager.cs
+++ Assets/AwesomeJumpNRun/Scripts/Manager/LightSwitchManager.cs
@@ -7,31 +7,44 @@ public class LightSwitchManager : MonoBehaviour {
     [SerializeField]
     public Light mylight;
     public GameObject[] players;
-    
 
     private void OnTriggerEnter(Collider other)
     {
 
         if (other.gameObject.CompareTag("Player"))
         {
-            players = GameObject.FindGameObjectsWithTag("Player");
+            //Turn off lights
             mylight.GetComponent<Light>().intensity = 0.0f;
+            //Make player a light
+            players = GameObject.FindGameObjectsWithTag("Player");
             foreach (GameObject player in players)
             {
                 player.GetComponent<Light>().intensity = 10.0f;
             }
+            //Make it black 
+            GameObject.Find("CaveWall1").transform.localScale = new Vector3(0.2f, 27.0f, 67.0f);
+            GameObject.Find("CaveWall2").transform.localScale = new Vector3(48.0f, 36.0f, 0.5f);
+            GameObject.Find("CaveWall3").transform.localScale = new Vector3(0.0001f, 27.0f, 20f);
+
         }
     }
 
     private void OnTriggerExit(Collider other) {
         if (other.gameObject.CompareTag("Player"))
         {
-            players = GameObject.FindGameObjectsWithTag("Player");
+            //Turn lights on
             mylight.GetComponent<Light>().intensity = 3.0f;
+            //Remove light from player
+            players = GameObject.FindGameObjectsWithTag("Player");
             foreach (GameObject player in players)
             {
                 player.GetComponent<Light>().intensity = 0.0f;
             }
+            //Make it bright
+            //Make it black 
+            GameObject.Find("CaveWall1").transform.localScale = new Vector3(0.0f, 0.0f, 0.0f);
+            GameObject.Find("CaveWall2").transform.localScale = new Vector3(0.0f, 0.0f, 0.0f);
+            GameObject.Find("CaveWall3").transform.localScale = new Vector3(0.0f, 0.0f, 0.0f);
         }
     }
 }
