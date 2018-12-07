113eebb4ca0b44f9bb73fd7a2589b1fc659235e7
Cramery <rami-602@hotmail.com>
2018-12-07 13:38:15 +0100

    Player is own light

--- Assets/AwesomeJumpNRun/Scripts/Manager/LightSwitchManager.cs
+++ Assets/AwesomeJumpNRun/Scripts/Manager/LightSwitchManager.cs
@@ -6,22 +6,32 @@ public class LightSwitchManager : MonoBehaviour {
 
     [SerializeField]
     public Light mylight;
-  
+    public GameObject[] players;
+    
 
     private void OnTriggerEnter(Collider other)
     {
 
         if (other.gameObject.CompareTag("Player"))
         {
-            mylight.GetComponent<Light>().intensity = 0.0f;   
+            players = GameObject.FindGameObjectsWithTag("Player");
+            mylight.GetComponent<Light>().intensity = 0.0f;
+            foreach (GameObject player in players)
+            {
+                player.GetComponent<Light>().intensity = 10.0f;
+            }
         }
     }
 
     private void OnTriggerExit(Collider other) {
-
         if (other.gameObject.CompareTag("Player"))
         {
+            players = GameObject.FindGameObjectsWithTag("Player");
             mylight.GetComponent<Light>().intensity = 3.0f;
+            foreach (GameObject player in players)
+            {
+                player.GetComponent<Light>().intensity = 0.0f;
+            }
         }
     }
 }
