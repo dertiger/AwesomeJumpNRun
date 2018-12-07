e57670f5ec9ad97e83e6ab8b492e7c6a51881eb4
Cramery <rami-602@hotmail.com>
2018-12-07 11:04:39 +0100

    Moving Plattformen

--- Assets/AwesomeJumpNRun/Scripts/Manager/MovingPlattformManager.cs
+++ Assets/AwesomeJumpNRun/Scripts/Manager/MovingPlattformManager.cs
@@ -3,14 +3,42 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class MovingPlattformManager : MonoBehaviour {
+    [SerializeField]
+    int moveSpeed;
+    [SerializeField]
+    string direction;
 
-	// Use this for initialization
-	void Start () {
+    bool collided;
+    Vector3 move;
+    // Use this for initialization
+    void Start () {
 		
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		
+        //Move
+        
+        switch (direction) {
+            case "x": move = new Vector3(moveSpeed, 0, 0) * Time.deltaTime;
+                break;
+            case "y": move = new Vector3(0, moveSpeed, 0) * Time.deltaTime;
+                break;
+            case "z": move = new Vector3(0, 0, moveSpeed) * Time.deltaTime;
+                break;
+        }
+        //Vector3 move = new Vector3(0, 0, moveSpeed) * Time.deltaTime;
+        if (collided)
+        {
+            this.transform.position -= move;
+        }
+        else {
+            this.transform.position += move;
+        }
+       
 	}
+
+    void OnCollisionEnter(Collision collision) {
+        collided = !collided;
+    }
 }
