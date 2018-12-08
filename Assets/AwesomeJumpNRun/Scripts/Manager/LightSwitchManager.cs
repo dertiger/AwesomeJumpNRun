24befa5845d4174532ef7eb96a0130a18f69637d
Kevin Huber <kevin.huber@hotmail.com>
2018-12-08 10:13:56 +0100

    Reversed ambient lighting back to normal. This is now changed in the code itself.

--- Assets/AwesomeJumpNRun/Scripts/Manager/LightSwitchManager.cs
+++ Assets/AwesomeJumpNRun/Scripts/Manager/LightSwitchManager.cs
@@ -14,6 +14,8 @@ public class LightSwitchManager : MonoBehaviour
         {
             cam.backgroundColor = Color.black;
             RenderSettings.fogColor = Color.black;
+            RenderSettings.ambientIntensity = 0;
+            RenderSettings.reflectionIntensity = 0;
             //Turn off lights
             mylight.GetComponent<Light>().intensity = 0.0f;
             //Make player a light
@@ -31,8 +33,10 @@ public class LightSwitchManager : MonoBehaviour
         {
             cam.backgroundColor = new Color(0.3490566f, 0.3490566f, 0.3490566f);
             RenderSettings.fogColor = new Color(0.3490566f, 0.3490566f, 0.3490566f);
+            RenderSettings.ambientIntensity = 1;
+            RenderSettings.reflectionIntensity = 1;
             //Turn lights on
-            mylight.GetComponent<Light>().intensity = 3.0f;
+            mylight.GetComponent<Light>().intensity = 1.0f;
             //Remove light from player
             players = GameObject.FindGameObjectsWithTag("Player");
             foreach (GameObject player in players)
