e9ad6728348a25809c67255ab35ae91d8a420446
Kevin Huber <kevin.huber@hotmail.com>
2018-12-08 10:03:26 +0100

    Changed the background in the cave.

--- Assets/AwesomeJumpNRun/Scripts/Manager/LightSwitchManager.cs
+++ Assets/AwesomeJumpNRun/Scripts/Manager/LightSwitchManager.cs
@@ -2,17 +2,18 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class LightSwitchManager : MonoBehaviour {
-
-    [SerializeField]
-    public Light mylight;
+public class LightSwitchManager : MonoBehaviour
+{
+    [SerializeField] private Light mylight;
+    [SerializeField] private Camera cam;
     public GameObject[] players;
 
     private void OnTriggerEnter(Collider other)
     {
-
         if (other.gameObject.CompareTag("Player"))
         {
+            cam.backgroundColor = Color.black;
+            RenderSettings.fogColor = Color.black;
             //Turn off lights
             mylight.GetComponent<Light>().intensity = 0.0f;
             //Make player a light
@@ -24,9 +25,12 @@ public class LightSwitchManager : MonoBehaviour {
         }
     }
 
-    private void OnTriggerExit(Collider other) {
+    private void OnTriggerExit(Collider other)
+    {
         if (other.gameObject.CompareTag("Player"))
         {
+            cam.backgroundColor = new Color(0.3490566f, 0.3490566f, 0.3490566f);
+            RenderSettings.fogColor = new Color(0.3490566f, 0.3490566f, 0.3490566f);
             //Turn lights on
             mylight.GetComponent<Light>().intensity = 3.0f;
             //Remove light from player
