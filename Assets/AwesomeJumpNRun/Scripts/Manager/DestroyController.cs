fb2e42fecc237744452d01fa16066dbae18cb9c0
Cramery <rami-602@hotmail.com>
2018-12-10 17:02:10 +0100

    Death Animantion

---
+++ Assets/AwesomeJumpNRun/Scripts/Manager/DestroyController.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestroyController : MonoBehaviour {
+
+    // Use this for initialization
+    void OnDestroy()
+    {
+        //Splitter the Destroyed Object
+        Debug.Log("OnDestroy1");
+    }
+}
