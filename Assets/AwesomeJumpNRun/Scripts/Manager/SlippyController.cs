6062e5bf48960623db24ab3414509e2be2ab28d6
Cramery <rami-602@hotmail.com>
2018-12-10 16:06:14 +0100

    Stuff

---
+++ Assets/AwesomeJumpNRun/Scripts/Manager/SlippyController.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SlippyController : MonoBehaviour {
+
+    private void Update() {
+        if (true) {
+
+        }
+    }
+    
+}
