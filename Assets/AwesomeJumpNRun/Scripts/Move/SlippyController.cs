64e100c76808614fb060772a5214f38a3f469bf8
Cramery <rami-602@hotmail.com>
2018-12-10 16:46:49 +0100

    Script for Ice added

---
+++ Assets/AwesomeJumpNRun/Scripts/Move/SlippyController.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SlippyController : MonoBehaviour {
+
+    public float thrust;
+    public Rigidbody rb;
+
+    void Start()
+    {
+        rb = GameObject.Find("Player").GetComponent<Rigidbody>();
+        thrust = 20.0f;
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyUp(KeyCode.RightArrow))
+        {
+            
+            rb.AddForce(0, 0, thrust, ForceMode.Impulse);
+        }
+       
+    }
+
+}
