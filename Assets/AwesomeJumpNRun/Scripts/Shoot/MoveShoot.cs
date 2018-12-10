d8907ace7a14863b901db7258e816cec067c3001
Daniel\Home <danielschumi@bluewin.ch>
2018-12-10 11:58:15 +0100

    destroy shoots after 10sec

--- Assets/AwesomeJumpNRun/Scripts/Shoot/MoveShoot.cs
+++ Assets/AwesomeJumpNRun/Scripts/Shoot/MoveShoot.cs
@@ -8,10 +8,17 @@ public class MoveShoot : MonoBehaviour {
     private void Start () {
         Rigidbody rigBody = GetComponent<Rigidbody>();
         rigBody.AddForce(transform.forward * speed);
+        StartCoroutine(TimebasedDestroy(10));
 	}
 
     private void OnCollisionEnter(Collision collision)
     {
+        Destroy(gameObject);
+    }
+
+    private IEnumerator TimebasedDestroy(int time)
+    {
+        yield return new WaitForSeconds(time);
         Destroy(gameObject);
     }
 }
