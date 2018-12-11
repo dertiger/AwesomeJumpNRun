a23aad502b9d731763a66c55612fcf336cfa3e13
Cramery <rami-602@hotmail.com>
2018-12-11 08:47:39 +0100

    Enemys shatter in pieces when destroyed

--- Assets/AwesomeJumpNRun/Scripts/Manager/DestroyController.cs
+++ Assets/AwesomeJumpNRun/Scripts/Manager/DestroyController.cs
@@ -4,10 +4,18 @@ using UnityEngine;
 
 public class DestroyController : MonoBehaviour {
 
+    public GameObject remains;
+
     // Use this for initialization
     void OnDestroy()
     {
         //Splitter the Destroyed Object
         Debug.Log("OnDestroy1");
+        Instantiate(remains, transform.position, transform.rotation);
+
+    }
+    void Update()
+    {
+
     }
 }
---
+++ Assets/AwesomeJumpNRun/Scripts/Manager/selfDestruction.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class selfDestruction : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        StartCoroutine(Waiting());
+    }
+
+    IEnumerator Waiting()
+    {
+        yield return new WaitForSeconds(2);
+        Destroy(gameObject);
+    }
+
+}
