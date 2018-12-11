87de03309f53331926e280a7df6e9fa5035f3b2c
Cramery <rami-602@hotmail.com>
2018-12-11 17:14:39 +0100

    LavaWorld Trigger

---
+++ Assets/AwesomeJumpNRun/Scripts/Manager/LavaWorld.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LavaWorld : MonoBehaviour {
+
+	void OnTriggerEnter () {
+		
+	}
+
+	void OnTriggerExit () {
+		
+	}
+}
