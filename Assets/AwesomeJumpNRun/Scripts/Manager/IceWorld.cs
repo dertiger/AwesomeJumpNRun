2998b22b7a345a1a53f34dcd8f075d5299c0701a
Cramery <rami-602@hotmail.com>
2018-12-12 09:14:09 +0100

    let it snow let it snow let it snow

---
+++ Assets/AwesomeJumpNRun/Scripts/Manager/IceWorld.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class IceWorld : MonoBehaviour
+{
+
+    private GameObject lightobject;
+    private Light light;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            lightobject = GameObject.Find("Directional Light");
+            light = lightobject.GetComponent<Light>();
+            Color lightblue = new Color32(0, 247, 255, 255);
+            light.color = lightblue;
+            
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            lightobject = GameObject.Find("Directional Light");
+            light = lightobject.GetComponent<Light>();
+            light.color = Color.white;
+        }
+    }
+}
