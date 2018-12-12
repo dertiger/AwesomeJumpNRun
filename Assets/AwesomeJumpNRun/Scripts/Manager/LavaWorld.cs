30769481d91685f5201d5c0ad879fd0c771e6024
Cramery <rami-602@hotmail.com>
2018-12-12 08:36:36 +0100

    Light in Lavaworld

--- Assets/AwesomeJumpNRun/Scripts/Manager/LavaWorld.cs
+++ Assets/AwesomeJumpNRun/Scripts/Manager/LavaWorld.cs
@@ -2,13 +2,30 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class LavaWorld : MonoBehaviour {
+public class LavaWorld : MonoBehaviour
+{
 
-	void OnTriggerEnter () {
-		
-	}
+    private GameObject lightobject;
+    private Light light;
 
-	void OnTriggerExit () {
-		
-	}
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            lightobject = GameObject.Find("Directional Light");
+            light = lightobject.GetComponent<Light>();
+            light.color = Color.red;
+            //light.intensity = 4.0f;
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            lightobject = GameObject.Find("Directional Light");
+            light = lightobject.GetComponent<Light>();
+            light.color = Color.white;
+        }
+    }
 }
