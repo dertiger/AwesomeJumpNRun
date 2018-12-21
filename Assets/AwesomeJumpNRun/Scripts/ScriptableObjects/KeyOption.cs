6525d79e56ce564251bf9b1acd0e10d6de4c185f
Daniel\Home <danielschumi@bluewin.ch>
2018-12-21 01:20:53 +0100

    fixed strange SO error

--- Assets/AwesomeJumpNRun/Scripts/ScriptableObjects/KeyOption.cs
+++ Assets/AwesomeJumpNRun/Scripts/ScriptableObjects/KeyOption.cs
@@ -2,14 +2,13 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-[CreateAssetMenu(fileName = "KeyOptions", menuName = "KeyOptions", order = 1)]
-public class KeyOption : ScriptableObject
-{
+[CreateAssetMenu(fileName ="KeyOption", menuName ="KeyOption", order =1)]
+public class KeyOption : ScriptableObject {
     public KeyCode JUMPKEY = KeyCode.Space;
     public KeyCode MOVERIGHT = KeyCode.RightArrow;
     public KeyCode MOVELEFT = KeyCode.LeftArrow;
-    public KeyCode PERVIOUSCHAR = KeyCode.W;
-    public KeyCode NEXTCHAR = KeyCode.Q;
+    public KeyCode PERVIOUSCHAR = KeyCode.S;
+    public KeyCode NEXTCHAR = KeyCode.A;
     public KeyCode ENTERDIMENSION = KeyCode.F;
     public KeyCode SHOOT = KeyCode.D;
     public KeyCode RESTART = KeyCode.R;
