08b8dafeca108445b73325eecbb78864af9bf288
Cramery <rami-602@hotmail.com>
2018-12-21 08:06:02 +0100

    Added TP, Changes Gegner

---
+++ Assets/AwesomeJumpNRun/Scripts/Manager/Teleport.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Teleport : MonoBehaviour {
+
+    public GameObject[] players;
+
+    // Use this for initialization
+    void OnTriggerEnter(Collider collider) {
+        if (collider.gameObject.CompareTag("Player"))
+        {
+            players = GameObject.FindGameObjectsWithTag("Player");
+            foreach (GameObject player in players)
+            {
+                player.transform.position = new Vector3(0.0f, 1.0f, -30.0f);
+            }
+        }
+        
+    }
+}
